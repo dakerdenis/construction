c15e4a1bdb7092bdf60d3e48a991a6c0d7c32ccb
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-06 12:06:44 +0400

    add

--- admin/admin.php
+++ admin/admin.php
@@ -11,7 +11,23 @@
 </head>
 <body>
     <div class="admin__panel__wrapper">
+        <!-- header admin panel-->
+        <div class="admin_panel_header">
+            <!--admin logo--->
+            <div class="admin__panel__logo">
+                <div class="admin_logo"></div>
+                <div class="admin__pagename">Firm name_logo</div>
+            </div>
 
+            <!--admin page login-->
+            <div class="admin__panel_loginpanel">
+                <div class="admin__panel_username">Admin</div>
+                <div class="admin__panel_login">
+                    <a href="#"></a>
+                </div>
+            </div>
+
+        </div>
     </div>
 </body>
 </html>
