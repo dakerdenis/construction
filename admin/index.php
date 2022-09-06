dca76f41e420598e67742c3a311f5bea6b14cb9d
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-06 11:41:15 +0400

    add

---
+++ admin/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin panel</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <!--ADMIN PANEL styles-->
+</head>
+<body>
+    <div class="admin__panel__wrapper">
+        <div class="admin__panel__block">
+            <div class="admin__panel__logo">
+
+            </div>
+            <form action="./vendor/login.php" class="admin__panel__form">
+                <div class="admin_input__block">
+                    <div class="admin_input_desc">Username:</div>
+                    <div class="admin_input_input">
+                        <input type="text" name="email" id="email">
+                    </div>
+                </div>
+
+                <div class="admin_input__block">
+                    <div class="admin_input_desc">Password:</div>
+                    <div class="admin_input_input">
+                        <input type="password" name="password" id="password">
+                    </div>
+                </div>
+
+                <div class="admin_input_block">
+                    <div class="admin_input_submit">
+                        <input type="submit" value="Войти">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
