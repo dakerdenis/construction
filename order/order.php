f985f51c29395dd2e2ffa903e6f5182cfe526b91
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-12 23:05:42 +0400

    add

--- order/order.php
+++ order/order.php
@@ -21,15 +21,32 @@ if (isset($_POST['amount'])) {
     <div class="make_order_container">
         <form class="make_order__form" method="post">
             <div class="make_order__desc">
-                Оформление заказа 
+                Оформление заказа
             </div>
             <div class="make_order__text">
-               Заполните форму и мы свяжемся с вами для дальнейшего обсуждения вашей покупки 
+                Заполните форму и мы свяжемся с вами для дальнейшего обсуждения вашей покупки
             </div>
 
             <!--элемент формы-->
             <div class="make__order__form_element">
-
+                <div class="make__order_element_desc">
+                    Мобильный телефон
+                </div>
+                <input type="number" class="make__order_element_input">
+            </div>
+            <!--элемент формы-->
+            <div class="make__order__form_element">
+                <div class="make__order_element_desc">
+                    Электронная почта
+                </div>
+                <input type="number" class="make__order_element_input">
+            </div>
+            <!--элемент формы-->
+            <div class="make__order__form_element">
+                <div class="make__order_element_desc">
+                    Ваше имя
+                </div>
+                <input type="number" class="make__order_element_input">
             </div>
 
         </form>
@@ -45,34 +62,30 @@ if (isset($_POST['amount'])) {
 
 
 
-<?php
-while ($row = mysqli_fetch_assoc($select__catalog)) {
-    $id     = $row['id'];
-    $name   = $row['name'];
-    $image  = $row['image'];
-    $desc   = $row['description'];
-    $type   = $row['type'];
-    $price  = $row['price'];
+    <?php
+    while ($row = mysqli_fetch_assoc($select__catalog)) {
+        $id     = $row['id'];
+        $name   = $row['name'];
+        $image  = $row['image'];
+        $desc   = $row['description'];
+        $type   = $row['type'];
+        $price  = $row['price'];
 
-    $total_price =  $price *  $_amount_items;
-
-
-    $_SESSION["name"] = $name;
-?>
-    <p><?php echo $id; ?></p>
-    <p><?php echo $name; ?></p>
-    <p><?php echo $desc; ?></p>
-    <p><?php echo $type; ?></p>
-    <p><?php echo $_amount_items; ?></p>
-
-    <p>Сумма к заказу: <?php echo $total_price; ?> AZN </p>
+        $total_price =  $price *  $_amount_items;
 
 
-<?php
-}
-?>
+        $_SESSION["name"] = $name;
+    ?>
+        <p><?php echo $id; ?></p>
+        <p><?php echo $name; ?></p>
+        <p><?php echo $desc; ?></p>
+        <p><?php echo $type; ?></p>
+        <p><?php echo $_amount_items; ?></p>
+
+        <p>Сумма к заказу: <?php echo $total_price; ?> AZN </p>
+
+
+    <?php
+    }
+    ?>
 </div>
-
-
-
-
